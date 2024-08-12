43f638a419567d15ad551d7da74593db2c6d3c14
Matthew Anderson Hendricks <mah237@cam.ac.uk>
2024-08-12 21:38:38 +0100

    fix errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,14 @@
-import { Alignment, Button, Classes, Divider, Navbar } from "@blueprintjs/core";
 import css from "./App.module.scss";
 import { Route, Routes } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
 import { ROOT_PATH } from "./constants/constants";
 import { About } from "./pages/About";
 import { Home } from "./pages/Home";
+import { AppNavbar } from "./components/AppNavbar";
 
 function App() {
-  console.log(Classes.DIVIDER);
-  const navigate = useNavigate();
   return (
     <div className={css.AppContainer}>
-      <Navbar className={css.Navbar} fixedToTop={true}>
-        <Navbar.Group align={Alignment.LEFT}>
-          <Navbar.Heading>Idmatt GameDev</Navbar.Heading>
-          <Navbar.Divider />
-          <Button
-            icon="home"
-            text="Home"
-            onClick={() => navigate(ROOT_PATH + "")}
-          />
-          <Navbar.Divider />
-          <Button
-            icon="person"
-            text="About"
-            onClick={() => navigate(ROOT_PATH + "/About")}
-          />
-        </Navbar.Group>
-      </Navbar>
+      #<AppNavbar></AppNavbar>
       <Routes>
         <Route path={ROOT_PATH + "/"} element={<Home></Home>} />
         <Route path={ROOT_PATH + "/About"} element={<About></About>} />
