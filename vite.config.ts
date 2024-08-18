265d89416a231ca1e91c1bc98d7891dda74174c3
Matthew Anderson Hendricks <mah237@cam.ac.uk>
2024-08-18 10:11:16 +0100

    try change packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import md from 'vite-plugin-md';
 import YAMLPlugin from "unplugin-yaml/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), md(), YAMLPlugin()],
+  plugins: [react(), YAMLPlugin()],
   assetsInclude: ['**/*.md'],
   base: '/idmattGameDevlog/'
 })
